28c22ad9ec77e175c569b4a1c66f898dbe56957a
baby <63604401+whoisbaby@users.noreply.github.com>
2021-10-13 21:50:39 +0300

    ✨

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
-
+ 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setPresence({
